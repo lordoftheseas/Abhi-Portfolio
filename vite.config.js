a3ebe5a0dc2fd0d83f7f9182d993fee8b6ffa995
Abhi <73391479+lordoftheseas@users.noreply.github.com>
2025-05-23 15:54:11 -0400

    Vite config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Abhi-Portfolio",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
