3d2c2340b161c088472c8c48b9da974d67cf83f6
Abhi <73391479+lordoftheseas@users.noreply.github.com>
2025-05-23 14:46:47 -0400

    Vercel fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Add any specific React plugin options here
+      include: "**/*.{jsx,tsx}",
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
